a94b628c194bf202f5fe9c012c9fd0113431d93f
Samir Mahmood <samirmalik7@hotmail.co.uk>
2025-11-22 11:03:56 +0000

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from '/react.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
@@ -24,7 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <p> Hello </p>
+        <p> Hello,  Dev</p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
